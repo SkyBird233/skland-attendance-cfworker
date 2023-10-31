88a5a31ed0dfe97e01d668902de12c4f9ef75c05
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2023-10-31 23:36:36 +0800

    Implement core functionality

--- src/worker.js
+++ src/worker.js
@@ -1,30 +1,46 @@
-/**
- * Welcome to Cloudflare Workers!
- *
- * This is a template for a Scheduled Worker: a Worker that can run on a
- * configurable interval:
- * https://developers.cloudflare.com/workers/platform/triggers/cron-triggers/
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	// The scheduled handler is invoked at the interval set in our wrangler.toml's
-	// [[triggers]] configuration.
 	async scheduled(event, env, ctx) {
-		// A Cron Trigger can make requests to other endpoints on the Internet,
-		// publish to a Queue, query a D1 Database, and much more.
-		//
-		// We'll keep it simple and make an API call to a Cloudflare API:
-		let resp = await fetch('https://api.cloudflare.com/client/v4/ips');
-		let wasSuccessful = resp.ok ? 'success' : 'fail';
+		// Headers from old Skland Android app
+		let sklandHeaders = new Headers({
+			'Content-Type': 'application/json',
+			'User-Agent': 'Skland/1.0.1 (com.hypergryph.skland; build: 100001014; Android 31; ) Okhttp/4.11.0',
+			vCode: '100001014',
+			vName: '1.0.1',
+			dId: 'de9759a5afaa634f',
+			platform: '1',
+			Cred: env.CRED,
+		});
 
-		// You could store this result in KV, write to a D1 Database, or publish to a Queue.
-		// In this template, we'll just log the result:
-		console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
+		// Get player bindings
+		const playerBinding = await fetch('https://zonai.skland.com/api/v1/game/player/binding', {
+			method: 'GET',
+			headers: sklandHeaders,
+		}).then((response) => response.json());
+		if (playerBinding.code != 0) {
+			console.log('Get player bindings error:', playerBinding);
+			return;
+		}
+
+		// Get all attandance info
+		let attandanceData = [];
+		for (const app of playerBinding.data.list) {
+			for (const binding of app.bindingList) {
+				attandanceData.push({
+					uid: binding.uid,
+					gameId: binding.channelMasterId,
+				});
+			}
+		}
+
+		// Attandance
+		for (const data of attandanceData) {
+			const attandanceRespose = await fetch('https://zonai.skland.com/api/v1/game/attendance', {
+				method: 'POST',
+				headers: sklandHeaders,
+				body: JSON.stringify(data),
+			}).then((response) => response.json());
+			if (attandanceRespose.code != 0) console.log('Attendance error:', attandanceRespose, data);
+			else console.log('Attendance success:', attandanceRespose, data);
+		}
 	},
 };
